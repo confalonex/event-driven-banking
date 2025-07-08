0cc112708f6e3c5ff01bff36ef2a245a8e9edf16
Alex <confalonealex@gmail.com>
2025-07-08 15:29:58 +0200

    Inizializzazione progettto Kafka Spring Boot

---
+++ src/main/java/it/alex/kafka/banking/EventDrivenBankingApplication.java
@@ -0,0 +1,13 @@
+package it.alex.kafka.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/alex/kafka/banking/EventDrivenBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.alex.kafka.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
